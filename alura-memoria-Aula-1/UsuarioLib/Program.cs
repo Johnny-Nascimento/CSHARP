91218f714c2733ae2933fe7efbabec38270e1c14
Johnny Anderson Nascimento <nascimentoho1998@gmail.com>
2025-04-03 15:52:37 -0300

    - Tstes de performance com struct.

--- alura-memoria-Aula-1/UsuarioLib/Program.cs
+++ alura-memoria-Aula-1/UsuarioLib/Program.cs
@@ -1,5 +1,7 @@
-﻿using UsuarioLib;
+﻿using System.Diagnostics;
+using UsuarioLib;
 
+/*
 Usuario usuario = 
     new Usuario(
         "Daniel", 
@@ -14,3 +16,18 @@ usuario.PadronizaTelefones();
 
 //912345678
 usuario.ExibeTelefones();
+*/
+
+Stopwatch sw = Stopwatch.StartNew();
+sw.Start();
+
+for (int i = 0; i < 1000000; i++)
+{
+    Coordenada coordenada = new Coordenada(123.456, -123.456);
+    var latitude = coordenada.Latitude;
+    var longitude = coordenada.Longitude;
+}
+
+sw.Stop();
+
+Console.WriteLine($"Tempo: {sw.Elapsed.TotalMilliseconds}");
--- alura-memoria-Aula-1/UsuarioLib/Usuario.cs
+++ alura-memoria-Aula-1/UsuarioLib/Usuario.cs
@@ -10,6 +10,7 @@ public class Usuario
         Nome = nome;
         Email = email;
         Telefones = telefone;
+        return;
 
         ChavesAcesso = new List<int>();
         Stopwatch stopwatch = new Stopwatch();
