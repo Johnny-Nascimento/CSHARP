58b0b95cc8f43912676ce780328d17cdc3cf8075
Johnny Anderson Nascimento <nascimentoho1998@gmail.com>
2025-04-04 15:29:59 -0300

    - Testes de performance de record e record structs

--- alura-memoria-Aula-1/UsuarioLib/Program.cs
+++ alura-memoria-Aula-1/UsuarioLib/Program.cs
@@ -16,7 +16,7 @@ usuario.PadronizaTelefones();
 
 //912345678
 usuario.ExibeTelefones();
-*/
+
 
 Stopwatch sw = Stopwatch.StartNew();
 sw.Start();
@@ -30,4 +30,47 @@ for (int i = 0; i < 1000000; i++)
 
 sw.Stop();
 
+Console.WriteLine($"Tempo: {sw.Elapsed.TotalMilliseconds}");
+
+
+FormularioDto dto = new FormularioDto("11111111111", 50, "Teste");
+dto.Nome = "Nome";
+Console.WriteLine(dto.Nome);
+dto.Nome = "Nome";
+Console.WriteLine(dto.Nome);
+
+FormularioDto dto2 = new FormularioDto();
+dto2.Nome = "Teste";
+dto2.Idade = 50;
+dto2.Cargo = "Teste";
+dto2.Cpf = "11111111111";
+
+Console.WriteLine(dto == dto2);
+*/
+
+Stopwatch sw = Stopwatch.StartNew();
+sw.Start();
+
+for (int i = 0; i < 1000000000; i++)
+{
+    Coordenada coordenada = new Coordenada(123.456, -123.456);
+    var latitude = coordenada.Latitude;
+    var longitude = coordenada.Longitude;
+}
+
+sw.Stop();
+Console.WriteLine($"Tempo: {sw.Elapsed.TotalMilliseconds}");
+
+
+sw.Restart();
+
+for (int i = 0; i < 1000000000; i++)
+{
+    FormularioDto dto = new FormularioDto("Teste", "11111111", 25, "Dess");
+
+    var nome = dto.Nome;
+    var cpf = dto.Cpf;
+}
+
+sw.Stop();
 Console.WriteLine($"Tempo: {sw.Elapsed.TotalMilliseconds}");
--- alura-memoria-Aula-1/UsuarioLib/UsuarioDto.cs
+++ alura-memoria-Aula-1/UsuarioLib/UsuarioDto.cs
@@ -1,8 +1,15 @@
 ﻿namespace UsuarioLib;
 
-public class UsuarioDto
+public readonly record struct FormularioDto(string Nome, string Cpf, int Idade, string Cargo);
+
+public record UsuarioDto
 {
     public string Nome { get; set; }
     public string Email { get; set; }
     public List<string> Telefones { get; set; }
+
+    public virtual bool Equals(UsuarioDto? usuario)
+    {
+        return this.Nome == usuario.Nome && this.Email == usuario.Email && this.Telefones.SequenceEqual(usuario.Telefones);
+    }
 }
