73057ee4eb011bd7944cdbaad29a4e3b85412f31
Johnny Anderson Nascimento <nascimentoho1998@gmail.com>
2025-04-16 13:57:25 -0300

    - Adicionado evento para mostrar mensagem de saldo insuficiente

--- EventsDelegatesLambda/Aula2/ByteBank.Console/CaixaEletronico.cs
+++ EventsDelegatesLambda/Aula2/ByteBank.Console/CaixaEletronico.cs
@@ -1,8 +1,12 @@
-﻿public class CaixaEletronico
+﻿public delegate void SaldoInsuficienteHandler();
+
+public class CaixaEletronico
 {
     private const int LarguraExtrato = 80;
     private decimal saldo;
     private List<ItemExtrato> itensExtrato = new();
+
+    private event SaldoInsuficienteHandler OnSaldoInsuficiente;
 
     public CaixaEletronico()
     {
@@ -15,6 +19,13 @@
             Sinal = SinalOperacao.Credito
         };
         itensExtrato.Add(item);
+
+        OnSaldoInsuficiente += MostraMensagemSaldoInsuficiente;
+    }
+
+    private void MostraMensagemSaldoInsuficiente()
+    {
+        Console.WriteLine("Saldo insuficiente.");
     }
 
     public void Saldo()
@@ -55,7 +66,7 @@
     {
         if (valor > saldo)
         {
-            Console.WriteLine("Saldo insuficiente.");
+            OnSaldoInsuficiente?.Invoke();
         }
         else
         {
@@ -78,7 +89,7 @@
     {
         if (valor > saldo)
         {
-            Console.WriteLine("Saldo insuficiente.");
+            OnSaldoInsuficiente?.Invoke();
         }
         else
         {
