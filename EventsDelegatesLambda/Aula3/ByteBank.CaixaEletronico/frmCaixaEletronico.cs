6ed9cb7c767e274a242ed7073a673f8a0bdeb4be
Johnny Anderson Nascimento <nascimentoho1998@gmail.com>
2025-04-16 19:05:24 -0300

    - Separação de responsabilidade com eventos.

--- EventsDelegatesLambda/Aula3/ByteBank.CaixaEletronico/frmCaixaEletronico.cs
+++ EventsDelegatesLambda/Aula3/ByteBank.CaixaEletronico/frmCaixaEletronico.cs
@@ -14,6 +14,15 @@
             btnDepositar.Click += BtnDepositar_Click;
             btnSaldo.Click += BtnSaldo_Click;
             btnExtrato.Click += BtnExtrato_Click;
+
+            caixaEletronico.OnDeposito += CaixaEletronico_OnDeposito;
+            caixaEletronico.OnSaque += CaixaEletronico_OnSaque;
+            caixaEletronico.OnSaldoInsuficiente += CaixaEletronico_OnSaldoInsuficiente;
+        }
+
+        private void CaixaEletronico_OnSaldoInsuficiente(object sender, TransacaoEventArgs e)
+        {
+            WriteToConsole($"Saldo insuficiente! Você tentou sacar {e.ValorTransacao} e o saldo é {e.Saldo}");
         }
 
         private void ImprimirLogo()
