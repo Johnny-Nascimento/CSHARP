545970a4f7905ed53ca33d7ac36999828f8ff9ae
Johnny Anderson Nascimento <nascimentoho1998@gmail.com>
2025-05-22 13:53:19 -0300

    - Adicionado configurações de cookies e de senha.

---
+++ MedVoll/MedVoll.Web/Areas/Identity/Constantes.cs
@@ -0,0 +1,8 @@
+﻿namespace MedVoll.Web.Areas.Identity
+{
+    public static class Constantes
+    {
+        public const int RequiredMaxLength = 100;
+        public const int RequiredMinLength = 8;
+    }
+}
